fef8b2afe2dd617b0ff80dc6cb3d14c2e625aff9
illugi317 <illugi@illugi.com>
2020-09-03 09:50:00 +0000

    Algorithim written for max_int

---
+++ max_int.py
@@ -0,0 +1,10 @@
+max_int = 0
+while True:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int < 0:
+        break
+    # Fill in the missing code
+    if num_int > max_int:
+        max_int = num_int
+
+print("The maximum is", max_int)    # Do not change this line
